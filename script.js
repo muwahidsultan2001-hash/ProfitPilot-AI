e7b625cd6841574d116c2148a0408c259a5475ca
muwahidsultan2001-hash <muwahidsultan2001@gmail.com>
2026-06-02 09:56:43 +0500

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ async function searchProducts() {
     pipelineDiv.innerHTML = "";
 
     try {
-        const response = await fetch("http://127.0.0.1:8000/search", {
+        const response = await fetch("https://profitpilot-ai-1.onrender.com/search", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
